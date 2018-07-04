8888e3acbd79d30a681e87ac7d1cd4ebb382ed8c
Sebastian Rodriguez <se.rodriguezlz@gmail.com>
2018-07-04 12:57:16 -0400

    Primer Commit

---
+++ OrdenCompra/OrdenCompra/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace OrdenCompra.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
